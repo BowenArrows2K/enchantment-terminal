d2229423d2db58172549287a6db40258b5e4c4c9
BowenArrows2K <bowen.quaid@gmail.com>
2024-06-22 16:40:34 -0500

    Added Item

---
+++ src/main/java/net/bowenarrrows/enchantmentterm/item/ModItemGroups.java
@@ -0,0 +1,23 @@
+package net.bowenarrrows.enchantmentterm.item;
+
+import net.bowenarrrows.enchantmentterm.EnchantmentTerminal;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup ENCHANT_TERM_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(EnchantmentTerminal.MOD_ID, "enchant-term"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.enchant_term"))
+                    .icon(() -> new ItemStack(ModItems.PORTABLE_ENCHANT_TERM)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.PORTABLE_ENCHANT_TERM);
+                    }).build());
+
+    public static void  registerItemGroups() {
+        EnchantmentTerminal.LOGGER.info("Registering Item Groups for " + EnchantmentTerminal.MOD_ID);
+    }
+}
---
+++ src/main/java/net/bowenarrrows/enchantmentterm/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.bowenarrrows.enchantmentterm.item;
+
+import net.bowenarrrows.enchantmentterm.EnchantmentTerminal;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PORTABLE_ENCHANT_TERM = registerItem("portable-enchant-term", new Item(new FabricItemSettings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(EnchantmentTerminal.MOD_ID, name), item);
+    }
+
+    public static void  registerModItems() {
+        EnchantmentTerminal.LOGGER.info("Registering Mod Items for " + EnchantmentTerminal.MOD_ID);
+    }
+}
