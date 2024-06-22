6a88b6c91c356e5aa95d0a18acb2823a449036cc
BowenArrows2K <bowen.quaid@gmail.com>
2024-06-22 15:45:41 -0500

    Initial commit

---
+++ src/main/java/net/bowenarrrows/enchantmentterm/EnchantmentTerminalClient.java
@@ -0,0 +1,10 @@
+package net.bowenarrrows.enchantmentterm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantmentTerminalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bowenarrrows/enchantmentterm/EnchantmentTerminalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bowenarrrows.enchantmentterm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnchantmentTerminalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
